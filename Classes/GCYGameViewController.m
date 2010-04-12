88299d8d966a42d59fb52b7afcc977644aa46743
lynlyn <lynlyn@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-04-12 02:11:10 +0000

    tapping buttons on yboard actually does stuff, but it still needs a primitive and better animation methods
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/iGamesman@4800 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- Classes/GCYGameViewController.m
+++ Classes/GCYGameViewController.m
@@ -15,17 +15,33 @@
 	switch (layers){
 		case 0:
 			self = [super initWithNibName:@"GCYBoardView0" bundle: nil];
+			
+			for (int i = 1; i < 16; i++){
+				UIButton *button = (UIButton *) [self.view viewWithTag: i];
+				[button addTarget: self	action: @selector(tapped:) forControlEvents: UIControlEventTouchUpInside];
+			} 
 			break;
 		case 1:
 			self = [super initWithNibName:@"GCYBoardView1" bundle: nil];
+			
+			for (int i = 1; i < 31; i++){
+				UIButton *button = (UIButton *) [self.view viewWithTag: i];
+				[button addTarget: self	action: @selector(tapped:) forControlEvents: UIControlEventTouchUpInside];
+			} 
 			break;
 		case 2:
 			self = [super initWithNibName:@"GCYBoardView2" bundle: nil];
+			
+			for (int i = 1; i < 49; i++){
+				UIButton *button = (UIButton *) [self.view viewWithTag: i];
+				[button addTarget: self	action: @selector(tapped:) forControlEvents: UIControlEventTouchUpInside];
+			} 
 			break;
 		default:
 			self = nil;
 			break;
 	}
+	
 	
 	return self;
 }
@@ -38,12 +54,32 @@
 		switch  (game.layers){
 			case 0:
 				self = [super initWithNibName:@"GCYBoardView0" bundle: nil];
+				
+//				for (int i = 1; i < 16; i++){
+//					UIView *button = [self.view viewWithTag: i];
+//					if ([button isKindOfClass: [UIButton class]]){
+//						NSLog(@"altering %d", i);
+//						[(UIButton *) button addTarget: self	action: @selector(tapped:) forControlEvents: UIControlEventTouchUpInside];
+//					}
+//				} 
 				break;
 			case 1:
 				self = [super initWithNibName:@"GCYBoardView1" bundle: nil];
+				
+//				for (int i = 1; i < 31; i++){
+//					UIView *button = [self.view viewWithTag: i];
+//					if ([button isKindOfClass: [UIButton class]])
+//						[(UIButton *) button addTarget: self	action: @selector(tapped:) forControlEvents: UIControlEventTouchUpInside];
+//				} 
 				break;
 			case 2:
 				self = [super initWithNibName:@"GCYBoardView2" bundle: nil];
+				
+//				for (int i = 1; i < 49; i++){
+//					UIView *button = [self.view viewWithTag: i];
+//					if ([button isKindOfClass: [UIButton class]])
+//						[(UIButton *) button addTarget: self	action: @selector(tapped:) forControlEvents: UIControlEventTouchUpInside];
+//				} 
 				break;
 			default:
 				self = nil;
@@ -55,20 +91,106 @@
 
 
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
+	[self disableButtons];
 }
-*/
 
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+
+- (void) doMove: (NSNumber *) move {
+	NSLog(@"do move: %d", move);
+	UIButton *B = (UIButton *) [self.view viewWithTag: [move integerValue]];
+	//[B retain];
+	//[B removeFromSuperview];
+	//[self.view insertSubview: B atIndex: 0];
+	//[B release];
+	//float B_width = B.frame.size.width;
+	//B.frame = CGRectMake(B.center.x - B_width / 4, B.center.y - B_width / 4, B_width / 2, B_width / 2);
+	[B setBackgroundImage: [UIImage imageNamed: (game.p1Turn ? @"C4X.png" : @"C4O.png")] forState: UIControlStateNormal];
+	
+	// do the board animations here (ie piece and connection animations)
+//	UIImageView *image = [[UIImageView alloc] initWithImage: (game.p1Turn? @"ConX.png" : @"ConO.png")];
+//	[UIView beginAnimations: @"Stretch" context: NULL];
+//	for (NSNumber *neighbor in [[game positionConnections] objectForKey: move]){
+//		int neighborAsInt = [neighbor integerValue];
+//		if (([[game getBoard] objectAtIndex: neighborAsInt] == X && game.p1Turn) || [[game getBoard] objectAtIndex: neighborAsInt] == O && !game.p1Turn){
+//			UIButton *neighborButton = (UIButton *) [self.view viewWithTag: neighborAsInt];
+//			image.frame = CGRectMake(<#CGFloat x#>, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)
+//		}
+//	}
+//	[UIView commitAnimations];
 }
-*/
+
+
+- (IBAction) tapped: (UIButton *) button{
+	NSLog(@"tapped");
+	NSNumber * num = [NSNumber numberWithInt: button.tag];
+	if([[game legalMoves] containsObject: num]){
+		NSLog(@"posting human move");
+		[game postHumanMove: num];
+	}
+}
+
+
+/** Convenience method for disabling all of the board's buttons. */
+- (void) disableButtons {
+	switch (game.layers){
+		case 0:
+			for (int i = 1; i < 16; i++){
+				UIView *button = (UIButton *) [self.view viewWithTag: i];
+				if ([button isKindOfClass: [UIButton class]])
+					[(UIButton *) button setEnabled: NO];
+			} 
+			break;
+		case 1:
+			for (int i = 1; i < 31; i++){
+				UIView *button = (UIButton *) [self.view viewWithTag: i];
+				if ([button isKindOfClass: [UIButton class]])
+					[(UIButton *) button setEnabled: NO];
+			} 
+			break;
+		case 2:
+			for (int i = 1; i < 49; i++){
+				UIView *button = (UIButton *) [self.view viewWithTag: i];
+				if ([button isKindOfClass: [UIButton class]])
+					[(UIButton *) button setEnabled: NO];
+			} 
+			break;
+		default:
+			break;
+	}
+}
+
+
+- (void) enableButtons {
+	switch (game.layers){
+		case 0:
+			for (int i = 1; i < 16; i++){
+				UIView *button = (UIButton *) [self.view viewWithTag: i];
+				if ([button isKindOfClass: [UIButton class]])
+					[(UIButton *) button setEnabled: YES];
+			} 
+			break;
+		case 1:
+			for (int i = 1; i < 31; i++){
+				UIView *button = (UIButton *) [self.view viewWithTag: i];
+				if ([button isKindOfClass: [UIButton class]])
+					[(UIButton *) button setEnabled: YES];
+			} 
+			break;
+		case 2:
+			for (int i = 1; i < 49; i++){
+				UIView *button = (UIButton *) [self.view viewWithTag: i];
+				if ([button isKindOfClass: [UIButton class]])
+					[(UIButton *) button setEnabled: YES];
+			} 
+			break;
+		default:
+			break;
+	}
+}
 
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
