48fe12155e1d201cff03f4838fd4e70b947c5dd7
lynlyn <lynlyn@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-04-15 21:22:52 +0000

    added more lovely gross dictionary things for which piece is on which edge
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/iGamesman@4820 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- Classes/GCYGame.h
+++ Classes/GCYGame.h
@@ -21,13 +21,26 @@
 	PlayMode gameMode;
 	NSNumber *humanMove;
 	NSDictionary *positionConnections;
+	NSDictionary *edgesForPosition;
+	NSArray *leftEdges;
 	
 	//ghettotastic until I figure out what to do. 
 	NSDictionary *layer0Connections;
 	NSDictionary *layer1Connections;
 	NSDictionary *layer2Connections;
+	
+	//More ghettotastiliciousness
+	NSDictionary *layer0edgesForPosition;
+	NSDictionary *layer1edgesForPosition;
+	NSDictionary *layer2edgesForPosition;
+	
+	NSArray *layer0leftEdges;
+	NSArray *layer1leftEdges;
+	NSArray *layer2leftEdges;
 }
 
+@property (nonatomic, retain) NSDictionary *edgesForPosition;
+@property (nonatomic, retain) NSArray *leftEdges;
 @property (nonatomic, retain) NSDictionary *positionConnections;
 @property (nonatomic, retain) NSString *player1Name, *player2Name;
 @property (nonatomic, assign) PlayerType player1Type, player2Type;
