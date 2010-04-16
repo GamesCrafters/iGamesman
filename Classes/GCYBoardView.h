f59da90e77e534706886c64c312ada934a44f279
lynlyn <lynlyn@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-04-16 00:33:49 +0000

    Stuff to let me draw lines in between nodes in the YGame
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/iGamesman@4828 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ Classes/GCYBoardView.h
@@ -0,0 +1,16 @@
+//
+//  GCYBoardView.h
+//  GamesmanMobile
+//
+//  Created by Class Account on 4/15/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GCYBoardView : UIView {
+	GCYGame *game;
+}
+
+@end
---
+++ Classes/GCYBoardView.m
@@ -0,0 +1,36 @@
+//
+//  GCYBoardView.m
+//  GamesmanMobile
+//
+//  Created by Class Account on 4/15/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "GCYBoardView.h"
+
+
+@implementation GCYBoardView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect {
+	[super drawRect:rect];
+	
+	
+    // Drawing code
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
